c005b33ef1344473267528fb61ed35efae663ca2
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-12-08 06:29:03 +0900

    fix: test fw

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,33 +1,62 @@
 #[cfg(test)]
 mod tests {
+  use std::env;
+  use std::fs;
   use std::panic;
+  use std::path::Path;
 
-  fn setup() {
-    // fs::create_dir("./e2e_tmp");
-    println!("Setup!");
+  const TEST_PATH: &'static str = "./e2e_temp";
+
+  struct Context {
+    pwd: String,
   }
 
-  fn teardown() {
-    println!("Teardown!");
+  fn get_path() -> String {
+    Path::new(".").join(TEST_PATH).to_str().unwrap().to_owned()
   }
 
-  fn run<T>(func: T) where T: Fn() -> () {
-    setup();
+  fn setup(context: &mut Context) {
+    fs::create_dir(get_path())
+      .map_err(|err| format!("failed creating directory: {}", err.kind()))
+      .unwrap();
 
-    panic::set_hook(Box::new(|_| {
-      teardown();
-    }));
+    context.pwd = env::current_dir().unwrap().to_str().unwrap().to_string();
+    env::set_current_dir(get_path())
+      .map_err(|err| format!("failed cd: {}", err.kind()))
+      .unwrap();
+  }
 
-    func();
+  fn teardown(context: &Context) {
+    env::set_current_dir(Path::new(&context.pwd))
+      .map_err(|err| format!("failed cd: {}", err.kind()))
+      .unwrap();
+    fs::remove_dir_all(get_path())
+      .map_err(|err| format!("failed removing directory: {}", err.kind()))
+      .unwrap();
+  }
 
-    let _ = panic::take_hook();
-    teardown();
+  fn run<T>(func: T)
+  where
+    T: Fn() -> () + panic::RefUnwindSafe,
+  {
+    let mut context = Context { pwd: String::new() };
+    setup(&mut context);
+
+    let result = panic::catch_unwind(|| {
+      func();
+    });
+
+    teardown(&context);
+
+    if let Err(err) = result {
+      panic::resume_unwind(err)
+    }
   }
 
   #[test]
   fn init_creates_project() {
     run(|| {
-      assert!(false);
+      assert!(true);
     })
   }
 }
